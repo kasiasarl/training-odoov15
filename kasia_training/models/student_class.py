59bf542135977c8ad4e8357bc716f22860fb992c
Aimé Jules <aimejules1@gmail.com>
2022-03-01 14:48:05 +0300

    Ajout compute

--- kasia_training/models/student_class.py
+++ kasia_training/models/student_class.py
@@ -7,5 +7,11 @@ class StudentClass(models.Model):
     _description = 'New Description'
 
     st_class = fields.Char(string='Class', required=True)
-    student_ids = fields.One2many('student.management','class_id')
-    student_count = fields.Integer()
+    student_ids = fields.One2many('student.management', 'class_id')
+    student_count = fields.Integer(compute='_compute_student_count')
+    student_age = fields.Integer(compute='_compute_student_count', store=True)
+
+    @api.depends('student_ids')
+    def _compute_student_count(self):
+        for item in self:
+            item.write({'student_count': len(item.student_ids), 'student_age': sum(item.student_ids.mapped('age'))})
