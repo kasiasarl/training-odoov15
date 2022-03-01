68ff2a2556072629eadedbf8a987c5d86bc7d10a
Aimé Jules <aimejules1@gmail.com>
2022-03-01 09:26:59 +0300

    Ajout chatter - notebook

--- kasia_training/models/student_management.py
+++ kasia_training/models/student_management.py
@@ -6,11 +6,13 @@ from odoo import models, fields, api
 class StudentManagement(models.Model):
     _name = 'student.management'
     _description = 'Student Management'
+    _inherit = ['mail.thread', 'mail.activity.mixin', 'image.mixin']
 
     name = fields.Char(string='Name', required=True)
     age = fields.Integer(string='Age',)
     sexe = fields.Selection([('m', 'M'), ('f', 'F')], string='Sexe', default='m')
     address = fields.Char(string='Address')
     phone = fields.Char(string='Phone')
-    image = fields.Image(string='Image')
+    image = fields.Image(string='Image', ir_attachment=True)
     birth_date = fields.Date(string='Birth date')
+    biographie = fields.Html(string='Biographie')
