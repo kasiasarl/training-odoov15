58ad60cf5dc0e59fb04992c133c5cf4672bda652
Aimé Jules <aimejules1@gmail.com>
2022-03-01 12:15:57 +0300

    Ajout bouton

--- kasia_training/models/course_management.py
+++ kasia_training/models/course_management.py
@@ -8,4 +8,10 @@ class CourseManagement(models.Model):
 
     name = fields.Char(string='Name', required='')
     course_date = fields.Datetime(string='Date', required=True)
-    partner_id = fields.Many2one('res.partner', string='Partner', domain="[('is_profesor','=', True)]")
+    course_end = fields.Datetime(string='End', required=True)
+    partner_id = fields.Many2one('res.partner', string='Partner', domain="[('is_profesor','=', True)]", required=True)
+    state = fields.Selection([('new', 'New'), ('validated', 'Validated'), ('end', 'End'), ('canceled', 'Canceled')],
+                             default='new')
+
+    def run_validate(self):
+        self.write({'state': 'validated'})
