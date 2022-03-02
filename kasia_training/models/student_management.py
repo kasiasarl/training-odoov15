ef5a5d80374ad01180ad300967c72f87bba07f1a
Aimé Jules <aimejules1@gmail.com>
2022-03-02 13:52:25 +0300

    groups

--- kasia_training/models/student_management.py
+++ kasia_training/models/student_management.py
@@ -9,7 +9,7 @@ class StudentManagement(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin', 'image.mixin']
 
     name = fields.Char(string='Name', required=True)
-    age = fields.Integer(string='Age', )
+    age = fields.Integer(string='Age')
     sexe = fields.Selection([('m', 'M'), ('f', 'F')], string='Sexe', default='m')
     address = fields.Char(string='Address')
     phone = fields.Char(string='Phone')
